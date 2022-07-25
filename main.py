20d1f3604a9f77aa53440d40a5065c988e6c8e8c
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-25 15:46:40 +0300

    Создла 3 функции: - создаёт короткую ссылку - проверять короткая ли ссылка - считает кол-во переходов по короткой ссылке

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def count_clicks(token, url):
+    auth_headers = {'Authorization': f'Bearer {token}'}
+    total_clicks_url = f'https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary'
+    response = requests.get(total_clicks_url, params={'units': -1}, headers=auth_headers)
+    count_clicks = response.json()['total_clicks']
+    return count_clicks
+
+
+def shorten_link(token, url):
+    auth_headers = {'Authorization': f'Bearer {token}'}
+    payloads = {"long_url": url}
+    response = requests.post('https://api-ssl.bitly.com/v4/shorten', json=payloads, headers=auth_headers)
+    response.raise_for_status()
+    bitlink = response.json()['id']
+    return bitlink
+
+
+def is_bitlink(token, url):
+    auth_headers = {'Authorization': f'Bearer {token}'}
+    is_bitlink_url = f'https://api-ssl.bitly.com/v4/bitlinks/{url}'
+    response = requests.get(is_bitlink_url, headers=auth_headers)
+    if response.ok:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    token = os.environ['TOKEN']
+    try:
+        some_lurl = str(input("Введите длинную ссылочку: "))
+        if is_bitlink(token, some_lurl):
+            print("Кликов по ссылке: ", count_clicks(token, some_lurl))
+        else:
+            print('Битлинк', shorten_link(token, some_lurl))
+    except requests.exceptions.HTTPError as exc:
+        print(exc)
